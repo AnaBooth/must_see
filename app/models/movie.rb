9c6261e81b7600dc3b54cdf6562971fac2ddebff
Raghu Betina <raghu@firstdraft.com>
2016-11-29 00:50:08 +0000

    Direct association between movies and castings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,10 @@
 class Movie < ApplicationRecord
   # Direct associations
 
+  has_many   :castings,
+             :foreign_key => "movies_id",
+             :dependent => :destroy
+
   belongs_to :director
 
   # Indirect associations
